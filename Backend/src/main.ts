40c763ba4394f494adf0641c7e03e68dd2279598
Tobias Fabricio Escobar <98720272+TobiasEscobar@users.noreply.github.com>
2026-01-29 00:19:19 -0300

    Implementacion CORS middleware en main.ts
    Add CORS middleware to handle preflight requests

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -5,6 +5,23 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule); 
 
+  app.use((req, res, next) => {
+    if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Origin', req.headers.origin);
+      res.header(
+        'Access-Control-Allow-Methods',
+        'GET,HEAD,PUT,PATCH,POST,DELETE',
+      );
+      res.header(
+        'Access-Control-Allow-Headers',
+        'Content-Type, Authorization',
+      );
+      res.header('Access-Control-Allow-Credentials', 'true');
+      return res.sendStatus(204);
+    }
+    next();
+  });
+  
   app.enableCors({
     origin: (origin, callback) => {
       const allowedOrigins = [
