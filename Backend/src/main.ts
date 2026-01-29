78559bd9d0906ea7e44f277bece7ce71ba1e2624
Tobias Fabricio Escobar <98720272+TobiasEscobar@users.noreply.github.com>
2026-01-29 00:30:22 -0300

    Refactor CORS handling in main.ts
    Removed custom CORS middleware and simplified CORS configuration.

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -5,37 +5,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule); 
 
-  app.use((req, res, next) => {
-    if (req.method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Origin', req.headers.origin);
-      res.header(
-        'Access-Control-Allow-Methods',
-        'GET,HEAD,PUT,PATCH,POST,DELETE',
-      );
-      res.header(
-        'Access-Control-Allow-Headers',
-        'Content-Type, Authorization',
-      );
-      res.header('Access-Control-Allow-Credentials', 'true');
-      return res.sendStatus(204);
-    }
-    next();
-  });
-  
   app.enableCors({
-    origin: (origin, callback) => {
-      const allowedOrigins = [
-        process.env.URL_LOCAL,
-        process.env.URL_VERCEL,
-      ].filter(Boolean);
-
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    origin: true,
     credentials: true,
   });
 
